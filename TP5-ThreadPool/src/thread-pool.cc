95929d6c69e65aff56d3afff4e15c42547b04a10
Fran Giavarini <127367870+frangiavarini@users.noreply.github.com>
2025-06-18 19:37:42 -0300

    lol

--- TP5-ThreadPool/src/thread-pool.cc
+++ TP5-ThreadPool/src/thread-pool.cc
@@ -1,17 +1,115 @@
-/**
- * File: thread-pool.cc
- * --------------------
- * Presents the implementation of the ThreadPool class.
- */
+
 
 #include "thread-pool.h"
 using namespace std;
 
-ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), done(false) {}
+ThreadPool::ThreadPool(size_t numThreads) 
+    : wts(numThreads), availableWorkers(0), done(false) {
+
+    for (size_t i = 0; i < numThreads; ++i) {
+        wts[i].ts = thread([this, i] { worker(i); });
+        availableWorkers.signal(); // hace a cada worker visible al dispatcher
+    }
+
+    dt = thread([this] { dispatcher(); });
+}
 
 
-void ThreadPool::schedule(const function<void(void)>& thunk) {}
+// void ThreadPool::schedule(const function<void(void)>& thunk) {
+//     {
+//         lock_guard<mutex> lock(queueLock);
+//         taskQueue.push(thunk);
+//     }
+//     dispatcherCV.notify_one();
+// }
 
-void ThreadPool::wait() {}
+void ThreadPool::schedule(const function<void(void)>& thunk) {
+    {
+        lock_guard<mutex> lock(queueLock);
+        if (done) return;  // ya se está destruyendo
+        taskQueue.push(thunk);
+    }
+    dispatcherCV.notify_one();
+}
 
-ThreadPool::~ThreadPool() {}
+void ThreadPool::dispatcher() {
+    while (true) {
+        function<void(void)> thunk;
+        {
+            unique_lock<mutex> lock(queueLock);
+            dispatcherCV.wait(lock, [this]() {
+                return done || !taskQueue.empty();
+            });
+
+            if (done && taskQueue.empty()) break;
+
+            thunk = taskQueue.front();
+            taskQueue.pop();
+        }
+
+        availableWorkers.wait(); // espera a que haya un worker libre
+
+        for (size_t i = 0; i < wts.size(); ++i) {
+            if (wts[i].available) {
+                wts[i].available = false;
+                wts[i].thunk = thunk;
+                {
+                    lock_guard<mutex> guard(waitLock);
+                    runningTasks++;
+                }
+                wts[i].sem.signal(); // le pasa el trabajo
+                break;
+            }
+        }
+    }
+}
+
+void ThreadPool::worker(int id) {
+    while (true) {
+        wts[id].sem.wait();
+
+        if (done && wts[id].thunk == nullptr) break;
+
+        wts[id].thunk();
+
+        wts[id].thunk = nullptr;
+        wts[id].available = true;
+
+        {
+            lock_guard<mutex> guard(waitLock);
+            runningTasks--;
+            if (runningTasks == 0 && taskQueue.empty()) {
+                waitCV.notify_all();
+            }
+        }
+
+        availableWorkers.signal(); // avisa al dispatcher que hay un worker libre
+    }
+}
+
+void ThreadPool::wait() {
+    unique_lock<mutex> lock(waitLock);
+    waitCV.wait(lock, [this]() {
+        return runningTasks == 0 && taskQueue.empty();
+    });
+}
+
+ThreadPool::~ThreadPool() {
+    wait();
+
+    done = true;
+    dispatcherCV.notify_all();
+
+    for (size_t i = 0; i < wts.size(); ++i) {
+        wts[i].sem.signal();  // desbloquea a cada worker
+    }
+
+    if (dt.joinable()) dt.join();
+
+    for (auto& w : wts) {
+        if (w.ts.joinable()) w.ts.join();
+    }
+}
+
+
+
